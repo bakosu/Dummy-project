0f86ab9eb748e4842e210eae9240ed21fbae3d3b
bakosu <ojonimi@gmail.com>
2015-05-20 21:02:28 -0700

    Initial Commit

---
+++ Dummy project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dummy project
+//
+//  Created by Ojonimi Bako on 5/20/15.
+//  Copyright (c) 2015 Ojonimi Bako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
